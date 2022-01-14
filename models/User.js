67f930d032e0a2bd28d5a0e6844338491c63a54f
trung.git <trunght1810@gmail.com>
2022-01-14 16:40:44 +0700

    Set up Express and Implement JWT authentication

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const moongoose = require("mongoose");
+
+const UserSchema = new moongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = moongoose.model("user", UserSchema);
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+//Connect DB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+// Define Routes
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profiles", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/post"));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
